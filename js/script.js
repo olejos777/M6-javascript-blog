051733d32706ec3f7f887b68f78fb1b3cb9230c1
olejos777 <olejniczakk.maciejj@gmail.com>
2020-09-26 11:52:12 +0200

    install ESLint

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 'use strict';
 
 const links = document.querySelectorAll('.titles a');
+const titles = document.querySelector('.titles');
 const articles = document.querySelectorAll('.posts article');
 
 const titleClickHandler = function (event) {
   event.preventDefault();
-  console.log(event);
   const clickedElement = this;
 
   /* remove class 'active' from all article links  */
@@ -30,7 +30,28 @@ const titleClickHandler = function (event) {
 
 };
 
-for (let link of links) {
-  link.addEventListener('click', titleClickHandler);
-};
 
+
+function generateTitleLinks() {
+  const linkLeftSide = '<li><a href="#';
+  const linkMiddleSide = '"><span>';
+  const linkRightSide = '</span></a></li>';
+
+  titles.innerHTML = '';
+
+  for (let article of articles) {
+    const articleId = article.getAttribute('id');
+    const articleTitle = article.querySelector('.post-title').innerHTML;
+    const linkHtml = linkLeftSide + articleId + linkMiddleSide + articleTitle + linkRightSide;
+
+    titles.innerHTML = titles.innerHTML + linkHtml;
+  }
+
+  const links = document.querySelectorAll('.titles a');
+
+  for (let link of links) {
+    link.addEventListener('click', titleClickHandler);
+  }
+}
+
+generateTitleLinks();
