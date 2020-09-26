e1a7c1cc25ab2bce27f99dd20e26cfbb340dd3d0
olejos777 <olejniczakk.maciejj@gmail.com>
2020-09-26 19:58:28 +0200

    add data-tags to each article

--- js/script.js
+++ js/script.js
@@ -8,24 +8,18 @@ const titleClickHandler = function (event) {
   event.preventDefault();
   const clickedElement = this;
 
-  /* remove class 'active' from all article links  */
   for (let link of links) {
     link.classList.remove('active');
   }
 
-  /* add class 'active' to the clicked link */
   clickedElement.classList.add('active');
 
-  /* remove class 'active' from all articles */
   for (let article of articles) {
     article.classList.remove('active');
   }
 
-  /* get 'href' attribute from the clicked link */
   const linkHref = clickedElement.getAttribute('href');
 
-  /* find the correct article using the selector (value of 'href' attribute) */
-  /* add class 'active' to the correct article */
   document.querySelector(linkHref).classList.add('active');
 
 };
@@ -55,3 +49,4 @@ function generateTitleLinks() {
 }
 
 generateTitleLinks();
+
